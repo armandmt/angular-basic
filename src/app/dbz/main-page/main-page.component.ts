b69e8cd9671332c29bda32ce3002431630ec539f
Armand Mata <amata3@xtec.cat>
2021-07-29 00:33:41 +0200

    dia3

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 
+
+
+interface Personatge {
+
+  nom: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent  {
 
+  personatges: Personatge [] = [
+    {
+      nom: "Goku",
+      poder: 15000
+     },
+     {
+       nom: "Vegeta",
+       poder: 8500
+     }
+    
+    ]
+  nou:Personatge = {
+    nom: '',
+    poder: 0
+  }
+
   afegir () {
 
-    console.log("Heyyy")
-  }
-  
+    if (this.nou.nom.trim().length === 0) {
+      return;
+    }
 
+    console.log(this.nou)
+
+
+    this.personatges.push(this.nou)
+    console.log(this.personatges)
+    //this.nou.nom=''  
+    //this.nou.poder=0
+
+    this.nou = {
+      nom:'',
+      poder:0
+    }
+  
+  }
 }
